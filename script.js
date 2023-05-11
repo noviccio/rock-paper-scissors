bf2e660df547c71bef506eb151140181db58bff4
noviccio <mnovicio2001@gmail.com>
2023-05-11 16:30:20 -0400

    added button functionality

--- script.js
+++ script.js
@@ -1,79 +1,113 @@
-function getRandomString(arr) {
-    //generates a random index based on array length
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    //return the random string from the array
-    return arr[randomIndex]; 
-}
+//getting buttons for DOM
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors"); 
+const pScore = document.getElementById("player-score");
+const cScore = document.getElementById("comp-score");
+
+ //store scores 
+ let compScore = 0;
+ let playerScore = 0;
+
+rockButton.addEventListener("click", function() {
+    // Code for when the rock button is clicked
+    game("rock");
+    
+  });
+  
+paperButton.addEventListener("click", function() {
+    // Code for when the paper button is clicked
+    game("paper");
+  });
+  
+scissorsButton.addEventListener("click", function() {
+    // Code for when the scissors button is clicked
+    game("scissors");
+  });
+
+
+
+  // game logic // 
 
 function getComputerChoice(){
     const gameArr = ['Rock', 'Paper', 'Scissors']; 
     const result = getRandomString(gameArr);
     return result;
 }
+// function getPlayerChoice(){
+   // const playerChoice = prompt("Your choice")
+   // const caseIns = playerChoice.toLowerCase();
+   // return caseIns;
+// }
 
-function getPlayerChoice(){
-    const playerChoice = prompt("Your choice")
-    const caseIns = playerChoice.toLowerCase();
-    return caseIns;
-}
 function playRound(playerSelection, computerSelection){
-    //PSEUDOCODE
-    //if computerSelection = playerSelection, return tie
-    //if computerSelection is Rock & playerSelection is Paper, 
-    //return playerSelection as winner 
-    //if computerSelection is Paper & playerSelection is Scissors
-    //return playerSelection as winner
-    //if computerSelection is Scissors & playerSelection is Paper
-    //return playerSelection as winner
-    //else return computerSelection as winner
-
+    
     const playerWin = ("You win! " + playerSelection + " beats " + computerSelection);
     const playerLose = ("You lose! " + computerSelection + " beats " + playerSelection)
     const playerTie = "It's a tie!";
 
     if(computerSelection.toLowerCase() === playerSelection.toLowerCase()){
         return playerTie; 
-}else if((computerSelection === "Rock") && (playerSelection == "paper")
-||(computerSelection === "Scissors") && (playerSelection == "rock")
-||(computerSelection === "Paper") && (playerSelection == "scissors")){
+    }else if((computerSelection === "Rock") && (playerSelection == "paper")
+            ||(computerSelection === "Scissors") && (playerSelection == "rock")
+            ||(computerSelection === "Paper") && (playerSelection == "scissors")){ 
         return playerWin;
-}else if((computerSelection === "Rock") && (playerSelection == "scissors")
-||(computerSelection === "Scissors") && (playerSelection == "paper")
-||(computerSelection === "Paper") && (playerSelection == "rock")){
+
+    }else if((computerSelection === "Rock") && (playerSelection == "scissors")
+            ||(computerSelection === "Scissors") && (playerSelection == "paper")
+            ||(computerSelection === "Paper") && (playerSelection == "rock")){
         return playerLose;
+    
     }
 }
-function game(){
-    //call playRound 5 times. 
-    //display results of each round
-    //display winner at the end of the 5 rounds 
-    let compScore = 0;
-    let playerScore = 0; 
-    const results = [];
-    while((playerScore < 5) && (compScore < 5)){ 
 
-        const playerSelection = getPlayerChoice(); 
+function game(playerSelection){
+ 
+        //call helper functions and declare variables 
         const computerSelection = getComputerChoice();
         const result = playRound(playerSelection,computerSelection);
-        results.push(result);
 
+        //determine output of round
         if(result === "It's a tie!"){
-            alert("It's a tie! " + "Score: " + playerScore + " to " + compScore)
+            console.log(playerScore)
+            console.log(compScore)
+            console.log("It's a tie! ");
+            console.log("Score: " + playerScore + " to " + compScore);
         }
         else if(result === ("You win! " + playerSelection + " beats " + computerSelection)){
             playerScore++; 
-            alert("You win this round, " + playerSelection + " beats " + computerSelection + ". Score: " + playerScore + " to " + compScore)
+            console.log(playerScore)
+            console.log(compScore)
+            console.log("You win this round, " + playerSelection + " beats " + computerSelection);
+            console.log("Score: " + playerScore + " to " + compScore);
         }else if(result === ("You lose! " + computerSelection + " beats " + playerSelection)){
             compScore++;
-            alert("You lose! " + computerSelection + " beats " + playerSelection + ". Score: " + playerScore + " to " + compScore)
+            console.log(playerScore)
+            console.log(compScore)
+            console.log("You lose! " + computerSelection + " beats " + playerSelection);
+            console.log("Score: " + playerScore + " to " + compScore);
         }
-    }
 
-    if(playerScore === 5){
-        alert("You Win! Final Score: " + playerScore + " to " + compScore)
-    }else
-        alert(("You Lose! Final Score: " + playerScore + " to " + compScore))
+    //display final score
+    if(playerScore >= 5){ 
+        console.log(playerScore)
+        console.log("You Win!")
+        console.log("Final Score: " + playerScore + " to " + compScore)
+    }
+    if (compScore >= 5){
+        console.log(compScore)
+        console.log("You Lose!")
+        console.log("Final Score: " + playerScore + " to " + compScore)
+    }
 
 }
 
-console.log(game());
+
+
+//helper function
+function getRandomString(arr) {
+    //generates a random index based on array length
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    //return the random string from the array
+    return arr[randomIndex]; 
+}
